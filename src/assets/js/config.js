3d79753d65d38ef69ba78a2cadd4118f6060f5c9
Dmitri Afonin <stealss7@gmail.com>
2017-06-22 23:26:38 +0300

    fix 2

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,6 +1,6 @@
 
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-        gcmAPIKey: ' AAAA2V-HJ0E:APA91bH6UjXIDGo1FgWBSlGrI1uAIVHMzN_eL5OmZW7sBaDXUUj35AUQ-5OMuYQnATL13T6itiohDxf5osBPHAn1UZy9L3WvCh3RTSIu4L23B3-xXi9qqdgX43VpCWg52P5TA1f_-ijo '
-//                gcmAPIKey: 'AIzaSyCn42EsYjGZNMmpe1V59SXfOIgy3KMkMfA'
+        // gcmAPIKey: ' AAAA2V-HJ0E:APA91bH6UjXIDGo1FgWBSlGrI1uAIVHMzN_eL5OmZW7sBaDXUUj35AUQ-5OMuYQnATL13T6itiohDxf5osBPHAn1UZy9L3WvCh3RTSIu4L23B3-xXi9qqdgX43VpCWg52P5TA1f_-ijo '
+               gcmAPIKey: 'AIzaSyCn42EsYjGZNMmpe1V59SXfOIgy3KMkMfA'
     };
